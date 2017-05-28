d55578d508265d2da06d9fd87275776fe72e72d5
Yusuf Musleh <yusuf.musleh@gmail.com>
2017-05-28 15:12:56 -0400

    Add get started functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
 	display_plans_and_prices();
 	display_features();
+
+	$("a").click(function(e) {
+	    var a_tag_clicked = e.target.id;
+	    if (a_tag_clicked.indexOf("get_started") >= 0) {
+			get_started(a_tag_clicked);
+	    }
+	});
+
 });
 
 
@@ -22,4 +30,12 @@ function display_features() {
 		});
 		$("#features_btn").fadeOut("fast");
 	});
+}
+
+function get_started(a_tag_clicked) {
+	$("#plans_and_prices_div").fadeOut("fast", function(){
+		$("#collect_emails_div").fadeIn("slow");
+		// $("#pnp_btn").fadeIn("fast");
+	});
+	$("#features_btn").fadeOut("fast");
 }
