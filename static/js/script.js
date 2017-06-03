f72fda2721642d986f35f4466ab13706f7c2ac99
Yusuf Musleh <yusuf.musleh@gmail.com>
2017-06-03 18:56:32 -0400

    Add script to setup Mixpanel for insights Determine selected plan for email and insights

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,8 @@
+// Global Variables
+var selected_plan = null;
+
 $(document).ready(function() {
+	mixpanel.track("Page Viewed");
 	display_plans_and_prices();
 	display_features();
 
@@ -13,10 +17,13 @@ $(document).ready(function() {
 		$('#notify_submit_btn').attr('disabled',true);
 		event.preventDefault();
 
+		var data = $("#form_notify_id").serializeArray(); // convert form to array
+		data.push({name: "selected_plan", value: selected_plan});
+
 		$.ajax({
 			type: $("#form_notify_id").attr('method'),
 			url: $("#form_notify_id").attr('action'),
-			data: $("#form_notify_id").serialize(),
+			data: $.param(data),
 			success: function (data) {
 				$("#form_notify_div").html("<p>thank you for submitting your information, we look forward to contacting you soon!</p>");
 				console.log('Submission was successful.');
@@ -33,6 +40,7 @@ $(document).ready(function() {
 
 function display_plans_and_prices() {
 	$("#pnp_btn").click(function() {
+		mixpanel.track("Plans and Prices Button Clicked");
 		$("#features_div").fadeOut("fast", function(){
 			$("#plans_and_prices_div").fadeIn("slow");
 			$("#features_btn").fadeIn("fast");
@@ -43,6 +51,7 @@ function display_plans_and_prices() {
 
 function display_features() {
 	$("#features_btn").click(function() {
+		mixpanel.track("Features Button Clicked");
 		$("#plans_and_prices_div").fadeOut("fast", function(){
 			$("#features_div").fadeIn("slow");
 			$("#pnp_btn").fadeIn("fast");
@@ -52,9 +61,21 @@ function display_features() {
 }
 
 function get_started(a_tag_clicked) {
+	if (a_tag_clicked == "get_started_p1") {
+		mixpanel.track("2.99% + 1QAR Plan Click");
+		selected_plan = "2.99% + 1QAR Plan";
+	}
+	else if (a_tag_clicked == "get_started_p2") {
+		mixpanel.track("2.65% + 0.75QAR Plan Click");
+		selected_plan = "2.65% + 0.75QAR";
+	}
+	else if (a_tag_clicked == "get_started_p3") {
+		mixpanel.track("Enterprise Plan Clicked");
+		selected_plan = "Enterprise"
+	}
+
 	$("#plans_and_prices_div").fadeOut("fast", function(){
 		$("#collect_emails_div").fadeIn("slow");
-		// $("#pnp_btn").fadeIn("fast");
 	});
 	$("#features_btn").fadeOut("fast");
 }
